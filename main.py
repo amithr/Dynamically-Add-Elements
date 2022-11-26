6b4b50d6dd036d38639878f48f165628e26619cb
ravindaa <ravindaa@oregonstate.edu>
2022-11-26 09:30:24 +0100

    Minor changes for bug when adding rows

--- main.py
+++ main.py
@@ -1,38 +1,43 @@
 import PySimpleGUI as sg
 
-def item_row(item_num):
+def create_row(row_counter, row_number_view):
     row =  [sg.pin(
         sg.Col([[
-            sg.Button("X", border_width=0, button_color=(sg.theme_text_color(), sg.theme_background_color()), key=('-DEL-', item_num)),
-            sg.Input(size=(20,1), key=('-DESC-', item_num)),
-            sg.Text(f'Row {item_num}', key=('-STATUS-', item_num))]],
-        key=('-ROW-', item_num)
+            sg.Button("X", border_width=0, button_color=(sg.theme_text_color(), sg.theme_background_color()), key=('-DEL-', row_counter)),
+            sg.Input(size=(20,1), key=('-DESC-', row_counter)),
+            sg.Text(f'Row {row_number_view}', key=('-STATUS-', row_counter))]],
+        key=('-ROW-', row_counter)
         ))]
     return row
 
 
 
 layout = [  [sg.Text('Add and "Delete" Rows From a Window', font='15')],
-            [sg.Column([item_row(1)], k='-ROW_PANEL-')],
-            [sg.Text("X", enable_events=True, k='Exit', tooltip='Exit Application'), 
-            sg.Text('+', enable_events=True, k='Add Item', tooltip='Add Another Item')]]
+            [sg.Column([create_row(0, 1)], k='-ROW_PANEL-')],
+            [sg.Text("Exit", enable_events=True, key='-EXIT-', tooltip='Exit Application'),
+            sg.Text("Refresh", enable_events=True, key='-REFRESH-', tooltip='Exit Application'),
+            sg.Text('+', enable_events=True, k='-ADD_ITEM-', tooltip='Add Another Item')]]
 
-window = sg.Window('Dynamically Adding Elements', layout,  use_default_focus=False, font='15', metadata=1)
+window = sg.Window('Dynamically Adding Elements', 
+    layout,  use_default_focus=False, font='15')
 
-
+row_counter = 0
+row_number_view = 1
 while True:
     event, values = window.read()
-    print(event, values)
-    if event == sg.WIN_CLOSED or event == 'Exit':
+    if event == sg.WIN_CLOSED or event == '-EXIT-':
         break
-    if event == 'Add Item':
-        window.metadata += 1
+    if event == '-ADD_ITEM-':
+        row_counter += 1
+        row_number_view += 1
+        print("Actual Row Number: ", row_counter)
+        print("Displayed Row Number: ", row_number_view)
         # Allows you to add items to a layout
         # These items cannot be deleted, but can be made invisible
-        window.extend_layout(window['-ROW_PANEL-'], [item_row(window.metadata)])
+        window.extend_layout(window['-ROW_PANEL-'], [create_row(row_counter, row_number_view)])
     elif event[0] == '-DEL-':
-            window.metadata -= 1
-            window[('-ROW-', event[1])].update(visible=False)
+        row_number_view -= 1
+        window[('-ROW-', event[1])].update(visible=False)
 window.close()
 
 
